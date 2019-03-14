f1b158a09668d4fa8e12ec2fa461af7e20c4ebb2
Saul Diaz <saul.diaz.v@gmail.com>
2019-03-14 00:41:24 -0600

    Filters

--- python/bpmonline.py
+++ python/bpmonline.py
@@ -20,7 +20,7 @@ class BPMonline:
         headers = {'Content-Type': 'application/json'}
         self.__session = requests.post(self.__bpmonline_url + self.__login_uri, headers=headers, json=self.__login_credentials)
     
-    def select(self, RootSchemaName, Columns):
+    def select_json(self, RootSchemaName, Columns, Filters = None):
         select_json = {
             "RootSchemaName":RootSchemaName,
             "OperationType":0,
@@ -51,10 +51,74 @@ class BPMonline:
                     }
                 }
             })
+
+        if (Filters != None):
+            if (Filters['items'] != None):
+                if (isinstance(Filters['items'], dict)):
+                    if (len(Filters['items']) > 0):
+
+                        LogicalOperatorType = 0
+                        if ('logicalOperation' in Filters):
+                            LogicalOperatorType = Filters['logicalOperation']
+                        
+                        select_json['filters'] = {
+                            'logicalOperation':0,
+                            'isEnabled':True,
+                            'filterType':6,
+                            'items':{
+                                'CustomFilters':{
+                                    'logicalOperation':LogicalOperatorType,
+                                    'isEnabled':True,
+                                    'filterType':6,
+                                    'items':{}
+                                }
+                            }
+                        }
+
+                        for Column, parameter in Filters['items'].items():
+                            # EQUAL
+                            comparisonType = 3
+                            if ('comparisonType' in parameter):
+                                comparisonType = parameter['comparisonType']
+
+                            dataValueType = 0
+                            if ('dataValueType' in parameter):
+                                dataValueType = parameter['dataValueType']
+
+                            value = ''
+                            if ('value' in parameter):
+                                value = parameter['value']
+                            
+                            select_json['filters']['items']['CustomFilters']['items'].update({
+                                'customFilter' + Column + '_PHP':{
+                                    'filterType':1,
+                                    'comparisonType':comparisonType,
+                                    'isEnabled':True,
+                                    'trimDateTimeParameterToDate':False,
+                                    'leftExpression':{
+                                        'expressionType':0,
+                                        'columnPath':Column
+                                    },
+                                    'rightExpression':{
+                                        'expressionType':2,
+                                        'parameter':{
+                                            'dataValueType':dataValueType,
+                                            'value':value
+                                        }
+                                    }
+                                }
+                            })
+
         
-        select_url = self.__bpmonline_url + '/0/dataservice/json/SyncReply/SelectQuery'
+        select_url = self.__bpmonline_url + self.__select_uri
         headers = {'Content-Type': 'application/json'}
         headers['BPMCSRF'] = self.__session.cookies.get_dict()['BPMCSRF']
 
         select_response = requests.post(select_url, headers=headers, cookies=self.__session.cookies, json=select_json)
-        return json.loads(select_response.text)
+        return select_response.text
+    
+    def select(self, RootSchemaName, Columns, Filters = None):
+        select_json = self.select_json(RootSchemaName, Columns, Filters)
+        return json.loads(select_json)
+    
+    
