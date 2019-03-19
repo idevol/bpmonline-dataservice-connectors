de8ee176f944a40d460b9e964bdeb65fa78d0b46
Saul Diaz <saul.diaz.v@gmail.com>
2019-03-19 09:53:00 -0600

    Update bpmonline.php

--- php/bpmonline.php
+++ php/bpmonline.php
@@ -455,7 +455,7 @@ class BPMonline
         $Filters = '00000000-0000-0000-0000-000000000000';
         
         // Or
-        
+
         $Filters = array(
             'logicalOperation' => 0,
             'items' => array(
